f626aba24b6f382637e7c402fd2c3a2aabc2f4cd
Anıl Bulsun <81930281+anilblsn@users.noreply.github.com>
2023-10-11 01:00:03 +0300

    v1
    v1

--- candySrc/src/App.js
+++ candySrc/src/App.js
@@ -1,228 +1,235 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Button, Text, TouchableOpacity } from 'react-native';
-// import { v4 as uuidv4 } from 'uuid'; // uuid kütüphanesini ekliyoruz
+import React, { useEffect, useState } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
+import ScoreBoard from './components/ScoreBoard';
 
-
-
-function randomColor() {
-  const colors = ['red', 'pink', 'blue', 'yellow'];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
-}
+const width = 8;
+const candyColors = [
+  require('./images/blue-candy.png'),
+  require('./images/orange-candy.png'),
+  require('./images/purple-candy.png'),
+  require('./images/red-candy.png'),
+  require('./images/yellow-candy.png'),
+  require('./images/green-candy.png'),
+];
+const blank = require('./images/blank.png');
 
 const App = () => {
-  const [gridSize, setGridSize] = useState(5)
-  const [grid, setGrid] = useState([]);
-  const [selectedBlock, setSelectedBlock] = useState(null);
-  const [matchedCounts, setMatchedCounts] = useState({
-    red: 0,
-    pink: 0,
-    blue: 0,
-    yellow: 0,
-  });
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+  const [firstSelectedSquare, setFirstSelectedSquare] = useState(null);
+  const [secondSelectedSquare, setSecondSelectedSquare] = useState(null);
+  const [scoreDisplay, setScoreDisplay] = useState(0);
+  const [selectedSquare, setSelectedSquare] = useState(null);
 
-  const createGrid = () => {
-    const newGrid = [];
-    for (let i = 0; i < gridSize; i++) {
-      const row = [];
-      for (let j = 0; j < gridSize; j++) {
-        row.push({
-          key: `${i}-${j}-${Math.random()}`,
-          color: randomColor(),
-        });
-      }
-      newGrid.push(row);
-    }
-    setGrid(newGrid);
-    setSelectedBlock(null);
-  };
-
-  const toggleGridSize = (size) => {
-    setGridSize(size);
-    createGrid();
-
-  };
-
-  // const createGrid = () => {
-  //   const newGrid = [];
-  //   for (let i = 0; i < 5; i++) {
-  //     const row = [];
-  //     for (let j = 0; j < 5; j++) {
-  //       row.push({
-  //         key: `${i}-${j}-${Math.random()}`, // uuid ile benzersiz bir anahtar oluşturuyoruz
-  //         color: randomColor(),
-  //       });
-  //     }
-  //     newGrid.push(row);
-  //   }
-  //   setGrid(newGrid);
-  //   setSelectedBlock(null);
-  // };
-
-  const toggleBlockSelection = (block) => {
-    // Eğer tıklanan kutu zaten seçili ise, seçimden çıkar.
-    if (selectedBlock && selectedBlock.key === block.key) {
-      setSelectedBlock(null);
-    } else if (!selectedBlock) {
-      // İlk kutu seçiliyor.
-      setSelectedBlock(block);
-    } else {
-      // İkinci kutu seçiliyor. Yer değiştirme işlemi yapılacak.
-      const updatedGrid = [...grid];
-
-      // Seçili kutuların indekslerini bul.
-      const firstBlockIndex = selectedBlock.key.split('-').map(Number);
-      const secondBlockIndex = block.key.split('-').map(Number);
-
-      // İki kutunun rengini ve anahtarını değiştir.
-      [updatedGrid[firstBlockIndex[0]][firstBlockIndex[1]].color, updatedGrid[secondBlockIndex[0]][secondBlockIndex[1]].color] = [updatedGrid[secondBlockIndex[0]][secondBlockIndex[1]].color, updatedGrid[firstBlockIndex[0]][firstBlockIndex[1]].color];
-      [updatedGrid[firstBlockIndex[0]][firstBlockIndex[1]].key, updatedGrid[secondBlockIndex[0]][secondBlockIndex[1]].key] = [updatedGrid[secondBlockIndex[0]][secondBlockIndex[1]].key, updatedGrid[firstBlockIndex[0]][firstBlockIndex[1]].key];
-
-      setGrid(updatedGrid);
-      setSelectedBlock(null);
-    }
-  };
-
-  const checkMatches = () => {
-    const matchedBlocks = new Set();
+  const [firstSelectedSquareIndex, setFirstSelectedSquareIndex] = useState(null);
+  const [secondSelectedSquareIndex, setSecondSelectedSquareIndex] = useState(null);
   
-    // Dikey ve yatay eşleşmeleri kontrol et
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        if (grid[i] && grid[i][j]) { 
-          let countVertical = 0;
-          let countHorizontal = 0;
-          let currentColor = grid[i][j].color;
-  
-          // Dikey eşleşmeleri kontrol et
-          for (let k = i; k < 5; k++) {
-            if (grid[k] && grid[k][j] && grid[k][j].color === currentColor) {
-              countVertical++;
-            } else {
-              break;
-            }
-          }
-  
-          // Yatay eşleşmeleri kontrol et
-          for (let k = j; k < 5; k++) {
-            if (grid[i] && grid[i][k] && grid[i][k].color === currentColor) {
-              countHorizontal++;
-            } else {
-              break;
-            }
-          }
-  
-          // En az 3 eşleşme varsa, kutuları eşleşenler listesine ekle.
-          if (countVertical >= 3) {
-            for (let k = i; k < i + countVertical; k++) {
-              matchedBlocks.add(grid[k][j]);
-            }
-          }
-  
-          if (countHorizontal >= 3) {
-            for (let k = j; k < j + countHorizontal; k++) {
-              matchedBlocks.add(grid[i][k]);
-            }
-          }
+
+
+
+
+
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i <= 39; i++) {
+        const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+        const decidedColor = currentColorArrangement[i]
+        const isBlank = currentColorArrangement[i] === blank
+
+        if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+            setScoreDisplay((score) => score + 4)
+            columnOfFour.forEach(square => currentColorArrangement[square] = blank)
+            return true
         }
+    }
+}
+
+const checkForRowOfFour = () => {
+  for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+      const isBlank = currentColorArrangement[i] === blank
+
+      if (notValid.includes(i)) continue
+
+      if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+          setScoreDisplay((score) => score + 4)
+          rowOfFour.forEach(square => currentColorArrangement[square] = blank)
+          return true
       }
+  }
+}
+
+const checkForColumnOfThree = () => {
+  for (let i = 0; i <= 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+      const isBlank = currentColorArrangement[i] === blank
+
+      if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+          setScoreDisplay((score) => score + 3)
+          columnOfThree.forEach(square => currentColorArrangement[square] = blank)
+          return true
+      }
+  }
+}
+
+const checkForRowOfThree = () => {
+  for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+      const isBlank = currentColorArrangement[i] === blank
+
+      if (notValid.includes(i)) continue
+
+      if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+          setScoreDisplay((score) => score + 3)
+          rowOfThree.forEach(square => currentColorArrangement[square] = blank)
+          return true
+      }
+  }
+}
+
+const moveIntoSquareBelow = () => {
+  for (let i = 0; i <= 55; i++) {
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+      const isFirstRow = firstRow.includes(i)
+
+      if (isFirstRow && currentColorArrangement[i] === blank) {
+          let randomNumber = Math.floor(Math.random() * candyColors.length)
+          currentColorArrangement[i] = candyColors[randomNumber]
+      }
+
+      if ((currentColorArrangement[i + width]) === blank) {
+          currentColorArrangement[i + width] = currentColorArrangement[i]
+          currentColorArrangement[i] = blank
+      }
+  }
+}
+
+// const handleClick = (index) => {
+//   if (firstSelectedSquare === null) {
+//     setFirstSelectedSquare(index);
+//     console.log(index);
+//   } else if (secondSelectedSquare === null) {
+//     setSecondSelectedSquare(index);
+//     // Burada swapSquares fonksiyonunu çağırın
+//     swapSquares();
+//   }
+// };
+
+
+
+
+
+//   const swapSquares = () => {
+//     if (firstSelectedSquare !== null && secondSelectedSquare !== null) {
+//       const newColorArrangement = [...currentColorArrangement];
+//       const temp = newColorArrangement[firstSelectedSquare];
+//       newColorArrangement[firstSelectedSquare] = newColorArrangement[secondSelectedSquare];
+//       newColorArrangement[secondSelectedSquare] = temp;
+  
+//       // Yeni renk düzenini kullanarak state'i güncelleyin.
+//       setCurrentColorArrangement(newColorArrangement);
+  
+//       setFirstSelectedSquare(null);
+//       setSecondSelectedSquare(null);
+//     }
+//   };
+
+// Seçilen kutuyu günceller
+const handleSquarePress = (index) => {
+  if (selectedSquare === null) {
+    // Eğer henüz bir kutu seçilmediyse, seçilen kutuyu güncelle
+    setSelectedSquare(index);
+  } else {
+    // İki kutu seçilmişse, kutuları değiştir
+    console.log("Hamle yapıldı: ", selectedSquare, " -> ", index);
+    swapSquares(selectedSquare, index);
+  }
+};
+
+
+
+const swapSquares = (selectedIndex, targetIndex) => {
+  if (selectedIndex !== null && targetIndex !== null) {
+    // Seçilen kutuların satır ve sütun numaralarını hesaplayın
+    const selectedRow = Math.floor(selectedIndex / width);
+    const selectedCol = selectedIndex % width;
+    const targetRow = Math.floor(targetIndex / width);
+    const targetCol = targetIndex % width;
+
+    // İki kutunun arasındaki mesafeyi hesaplayın
+    const rowDiff = Math.abs(selectedRow - targetRow);
+    const colDiff = Math.abs(selectedCol - targetCol);
+
+    // Sadece bir adım yanında veya üstünde olan hareketleri kabul edin
+    if ((rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1)) {
+      const updatedArrangement = [...currentColorArrangement];
+      [updatedArrangement[selectedIndex], updatedArrangement[targetIndex]] = [
+        updatedArrangement[targetIndex],
+        updatedArrangement[selectedIndex],
+      ];
+      setCurrentColorArrangement(updatedArrangement);
+      setSelectedSquare(null); // Seçili kutuyu sıfırla
+    } else {
+      // İşlem geçersizse, seçili kutuyu sıfırlayın
+      setSelectedSquare(null);
     }
-  
-    // Eşleşen kutuları yerine yeni random renkli kutular ekleyin.
-    if (matchedBlocks.size > 0) {
-      const updatedGrid = [...grid];
-      [...matchedBlocks].forEach((block) => {
-        const [row, col] = block.key.split('-').map(Number);
-        updatedGrid[row][col] = {
-          key: block.key,
-          color: randomColor(),
-        };
-      });
-  
-      // Konsola kaç kutu eşleştiğini yazdır
-      console.log(`Eşleşen Kutu Sayısı: ${matchedBlocks.size}`);
-  
-      setGrid(updatedGrid);
-  
-      // Renk bazında eşleşme sayısını güncelle.
-      const updatedCounts = { ...matchedCounts };
-      [...matchedBlocks].forEach((block) => {
-        updatedCounts[block.color]++;
-      });
-      setMatchedCounts(updatedCounts);
+  }
+};
+
+
+
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
     }
+    setCurrentColorArrangement(randomColorArrangement);
   };
-  
-
-  // Her adımda eşleşmeleri kontrol et
-  useEffect(() => {
-    checkMatches();
-  }, [grid]);
 
   useEffect(() => {
-    createGrid();
+    createBoard();
+    console.log('ilk çalışma')
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Button title="Müzik" onPress={() => playMusic()} />
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForRowOfFour();
+      checkForColumnOfThree();
+      checkForRowOfThree();
+      moveIntoSquareBelow();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [currentColorArrangement]);
 
-      <Button title="3x3 Tablo" onPress={() => toggleGridSize(3)} />
-      <Button title="5x5 Tablo" onPress={() => toggleGridSize(5)} />
-      {/* <Text style={styles.matchedText}>Eşleşen Kutu Sayısı:</Text>
-      {Object.keys(matchedCounts).map((color) => (
-        <Text key={color} style={styles.matchedText}>
-          {color}: {matchedCounts[color]}
-        </Text>
-      ))} */}
-      <View style={styles.gridContainer}>
-        {grid.map((row, rowIndex) => (
-          <View key={rowIndex} style={styles.row}>
-            {row.map((block) => (
-              <TouchableOpacity
-                key={block.key} // Anahtar olarak benzersiz bir anahtar kullanıldı
-                style={[
-                  styles.box,
-                  { backgroundColor: block.color },
-                ]}
-                onPress={() => toggleBlockSelection(block)}
-              >
-                <Text style={styles.boxText}>a</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
+ 
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {currentColorArrangement.map((candyColor, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => handleSquarePress(index)}
+            style={{ width: 50, height: 50 }}
+          >
+            <Image source={candyColor} style={{ width: '100%', height: '100%' }} />
+          </TouchableOpacity>
         ))}
       </View>
+      <ScoreBoard score={scoreDisplay} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  gridContainer: {
-    marginTop: 20,
-  },
-  row: {
-    flexDirection: 'row',
-  },
-  box: {
-    width: 50,
-    height: 50,
-    margin: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boxText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  matchedText: {
-    fontWeight: 'bold',
-  },
-});
-
 export default App;
+
+
+
